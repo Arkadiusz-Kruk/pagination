52179aa420a33982d3240f4653e813ff78f66416
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-07-16 14:10:30 +0200

    a
    a

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -16,8 +16,9 @@ function renderPagination(data) {
   arrayOfPages = [...Array(data.page.totalPages).keys()]
   // console.log(arrayOfPages);
       visibleArrayOfPages = arrayOfPages.map((i) =>
-      `<button type="submit" value=${i} id=${i} class=pagination__btn>${i +1}</button>`).join("")-1;
+      `<button type="submit" value=${i} id=${i} class=pagination__btn>${i +1}</button>`).join("");
   paginationArray.innerHTML = visibleArrayOfPages;
+  visibleArrayOfPages<=5;
 }
 
 function currentPage(data) {
