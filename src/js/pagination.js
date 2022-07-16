ddae831e858562900c6d066b942606232405abec
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-07-16 13:26:08 +0200

    a
    a

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,25 +1,51 @@
+let pageNr = 0;
+let arrayOfPages = [];
+let visibleArrayOfPages;
+const paginationArray = document.querySelector(".pagination");
+function pagination(data) {
+  renderPagination(data);
+  currentPage(data);
+  changePage(data);
+  // console.log(data.page.totalElements);
+  // console.log(data.page.size);
+  // console.log(data.page.totalPages);
+  // console.log(data.page.number);
+};
 
-import Pagination from 'tui-pagination';
-import apiService from '../services/api-services';
-import { renderGallery, setEventsOnPage } from '../js/searching-input-dropdown';
-import { hideLoader, showLoader } from './preloader';
-
-function setPagination(totalEvents) {
-  const options = {
-    totalItems: totalEvents > 1000 ? 1000 : totalEvents,
-    itemsPerPage: apiService.size,
-    visiblePages: window.outerWidth < 768 ? 3 : 5,
-    page: 1,
-    centerAlign: true,
-  };
-  const pagination = new Pagination('pagination', options);
-
-  pagination.on('beforeMove', function (eventData) {
-    apiService.page = eventData.page - 1;
-    showLoader();
-    setEventsOnPage();
-    apiService.fetchEvent().then(renderGallery).catch(console.log).finally(hideLoader);
-  });
+function renderPagination(data) {
+  arrayOfPages = [...Array(data.page.totalPages).keys()]
+  // console.log(arrayOfPages);
+      visibleArrayOfPages = arrayOfPages.map((i) =>
+      `<button type="submit" value=${i} id=${i} class=pagination__btn>${i + 1}</button>`).join("");
+  paginationArray.innerHTML = visibleArrayOfPages;
 }
 
-export default setPagination;
+function currentPage(data) {
+  const currentPage = document.getElementById(data.page.number);
+  currentPage.classList.add("pagination__btn--active");
+}
+
+function changePage(data) {
+  for (let i = 0; i < data.page.totalPages; i++) {
+    let newPage = document.getElementById(i);
+    newPage.addEventListener("click", handleChangePage)
+  };
+  visiblePages: window.outerWidth < 768 ? 3 : 5,
+  page: 1,
+  centerAlign: true,
+  
+}
+
+function handleChangePage(event) {
+  event.preventDefault();
+  pageNr = event.target.value;
+  console.log(pageNr);
+  
+};
+
+// totalElements = ${page.totalElements} - liczba wszystkich elementów
+// size = ${page.size} - liczba elementów na stronie
+// totalPages = ${page.totalPages} - liczba wszystkich stron
+// number = &{page.number} - aktualna strona
+
+export { pagination, pageNr };
